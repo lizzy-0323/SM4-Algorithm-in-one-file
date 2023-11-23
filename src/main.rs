1d4a5cb033f5aec11f2497999b7b9f508a09bb24
lazy1 <674194901@qq.com>
2023-11-23 21:36:44 +0800

    complte sm4 algorithm

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+mod sm4;
+use sm4::SM4;
+fn main() {
+    println!("SM4 algorithm init");
+    let key = 0x0123456789abcdeffedcba9876543210;
+    let sm = SM4::new(key);
+    let plaintext = 0x0123456789abcdeffedcba9876543210;
+    println!("plaintext = {:x}", plaintext);
+    let ciphertext = sm.encrypt(plaintext);
+    println!("ciphertext = {:x}", ciphertext);
+    let decrypttext: u128 = sm.decrypt(ciphertext);
+    println!("decrypttext = {:x}", decrypttext)
+}
